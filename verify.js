a8eb68efb6cfdf45ebc474c3f967e4243b62afa4
uccasino <66396998+uccasino@users.noreply.github.com>
2020-07-23 11:57:38 +0900

    Create verify.js

---
+++ verify.js
@@ -0,0 +1,6 @@
+function verifyDragonTiger(hash, salt): gameResult {
+      const hashList = makeHashList(hash, salt);
+      const eight-deck-shoe = makeShoe(hashList);
+      const gameResult = simulateDragonTiger(eight-deck-shoe);
+      return gameResult;
+}
